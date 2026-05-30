f68456e1e1ba55602b4298a0090bf8ab5743f5d8
Ryota-Mikuni <0601szpubbb@gmail.com>
2026-05-30 23:37:10 +0900

    Add Cygnus gesture dt-bindings header

---
+++ config/include/dt-bindings/zmk/cygnus_gesture.h
@@ -0,0 +1,11 @@
+/*
+ * Cygnus gesture kind constants.
+ *
+ * These are passed to &cy_gesture in the AutoMouse layer.
+ */
+
+#pragma once
+
+#define CYG_GESTURE_D 1
+#define CYG_GESTURE_M 2
+#define CYG_GESTURE_G 3
